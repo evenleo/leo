2d8635fd5ebcca5948a5b2de6e36b93e56f0aa63
evenleo <1293173298@qq.com>
2020-08-26 20:36:14 +0800

    fix

--- src/rpc/test/rpc_client_test.cpp
+++ src/rpc/test/rpc_client_test.cpp
@@ -3,7 +3,7 @@
 
 using namespace leo;
 
-void result(std::string responese)
+void StrcatResult(std::string responese)
 {
     Serializer s(responese.c_str(), responese.size());
     response_t<std::string> res;
@@ -11,11 +11,19 @@ void result(std::string responese)
     std::cout << "code=" << res.code() << ", message=" << res.message() << ", value=" << res.value() << std::endl;
 }
 
+void addResult(std::string responese)
+{
+    Serializer s(responese.c_str(), responese.size());
+    response_t<int> res;
+    s >> res;
+    std::cout << "code=" << res.code() << ", message=" << res.message() << ", value=" << res.value() << std::endl;
+}
+
 int main(int argc, char** argv)
 {
     RpcClient client("127.0.0.1", 5000);
-    client.call<std::string>("Strcat", result, "even", 24);
-    client.call<int>("add", result, 10, 21);
+    client.call<std::string>("Strcat", StrcatResult, "even", 24);
+    client.call<int>("add", addResult, 10, 21);
 
     while (1) {
         sleep(1);
